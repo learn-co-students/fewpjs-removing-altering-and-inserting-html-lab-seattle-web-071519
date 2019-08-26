ae9ea197da195e736c3d5fd126b8aea4a336c048
marshallhelenm <marshallhelenm@gmail.com>
2019-08-26 13:12:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "HELEN is the champion";
+newHeader.class = "victory";
+
